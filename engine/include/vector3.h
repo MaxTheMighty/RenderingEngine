7917a65a8dd5cdf8ac2ace17c4de7539f300eafa
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-03 16:35:19 -0800

    fixed indenting for vector3.h

--- engine/include/vector3.h
+++ engine/include/vector3.h
@@ -8,46 +8,73 @@
 #include "tuple3.h"
 #include "point3.h"
 #include "normal3.h"
+#include "util.h"
 #include "tuplelength.h"
 #include <cmath>
 template <typename T>
 
 class Vector3 : public Tuple3<Vector3, T> {
     public:
-      Vector3(): Tuple3<Vector3, T>(){};
-      Vector3(T x, T y, T z): Tuple3<Vector3, T>(x,y,z){};
+        Vector3(): Tuple3<Vector3, T>(){};
+        Vector3(T x, T y, T z): Tuple3<Vector3, T>(x,y,z){};
 
-      template <typename U>
-      explicit Vector3(Vector3<U> v) : Tuple3<Vector3, T>(T(v.x), T(v.y), T(v.z)){};
+        template <typename U>
+        explicit Vector3(Vector3<U> v) : Tuple3<Vector3, T>(T(v.x), T(v.y), T(v.z)){};
 
-      template <typename U>
-      explicit Vector3(Point3<U> p) : Tuple3<Vector3,T>(T(p.x),T(p.y),T(p.z)){};
+        template <typename U>
+        explicit Vector3(Point3<U> p) : Tuple3<Vector3,T>(T(p.x),T(p.y),T(p.z)){};
 
-      template <typename U>
-      explicit Vector3(Normal3<U> n) : Tuple3<Vector3,T>(T(n.x),T(n.y),T(n.z)){};
+        template <typename U>
+        explicit Vector3(Normal3<U> n) : Tuple3<Vector3,T>(T(n.x),T(n.y),T(n.z)){};
 
-      static T LengthSquared(Vector3<T> v) {
+        static T LengthSquared(Vector3<T> v) {
           return (v.x*v.x) + (v.y*v.y) + (v.z * v.z);
-      }
+        }
 
-      static auto Length(const Vector3<T> v) -> typename TupleLength<T>::type{
+        static auto Length(const Vector3<T> v) -> typename TupleLength<T>::type{
           return std::sqrt(LengthSquared(v));
-      }
+        }
 
-      // In this situation, we use AUTO because the return type is defined by the division operator
-      // For the underlying Tuple3 class (using decltype magic)
-      static auto Normalize(const Vector3<T> v) {
+        // In this situation, we use AUTO because the return type is defined by the division operator
+        // For the underlying Tuple3 class (using decltype magic)
+        static auto Normalize(const Vector3<T> v) {
           return v / Length(v);
-      }
+        }
 
-      static T Dot(Vector3<T> v, Vector3<T> w) {
-          return {(v.x * w.x) + (v.y * w.y) + (v.z * w.z)};
-      }
+        static T Dot(Vector3<T> v, Vector3<T> w) {
+          return (v.x * w.x) + (v.y * w.y) + (v.z * w.z);
+        }
 
-      static T CalculateAngleBetween(Vector3<T> v, Vector3<T> w) {
+        // Works only for normalized vectors
+        static T CalculateAngleBetween(Vector3<T> v, Vector3<T> w) {
           // Look into why PBRT does it differently
-          return (std::acos(Dot(v,w) / (Length(v) * Length(w)))) * (180/M_PI);
-      }
+          // return (std::acos(Dot(v,w) / (Length(v) * Length(w)))) * (180/M_PI);
+          if (Dot(v, w) < 0)
+              return M_PI - 2 * SafeASin(Length(v + w) / 2);
+          else
+              return 2 * SafeASin(Length(w - v) / 2);
+        }
+
+        static T AbsDot(Vector3<T> v, Vector3<T> w) {
+          return std::abs(Dot(v,w));
+        }
+
+        // Vector W must be normalized
+        static Vector3<T> GramSchmidt(Vector3<T> v, Vector3<T> w) {
+          return v - Dot(v, w) * w;
+        }
+
+        static Vector3<T> FMA(Vector3<T> a, Vector3<T> b, Vector3<T> c) {
+          return (a*b) + c;
+        }
+        static Vector3<T> CrossProduct() {
+
+        }
+
+
+
+
+
 
 
 };
