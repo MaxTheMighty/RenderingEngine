bdc3680d8468fa93024aac28ff0ed3ee7176b43b
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-03 16:35:19 -0800

    fixed indenting for vector3.h, wrote new functions for Vector3 and related tests

--- tests/src/vector_tests.cpp
+++ tests/src/vector_tests.cpp
@@ -92,8 +92,7 @@ BOOST_AUTO_TEST_CASE(Vector3DivisionInplace) {
 BOOST_AUTO_TEST_CASE(PointToVector3) {
     Point3f point(1.1f,2.2f,3.3f);
     Vector3f vec3f(point);
-    BOOST_CHECK(vec3f == Vector3f(1.1f,2.2f,3.3f));
-}
+    BOOST_CHECK(vec3f == Vector3f(1.1f,2.2f,3.3f));}
 
 BOOST_AUTO_TEST_CASE(NormalToVector3) {
     Normal3f norm3f(1.1f,2.2f,3.3f);
@@ -122,12 +121,40 @@ BOOST_AUTO_TEST_CASE(Vector3Dot) {
 }
 
 BOOST_AUTO_TEST_CASE(Vector3Angle) {
-    Vector3f vec3fa(4,2,1);
-    Vector3f vec3fb(3,-1,5);
-    float angle = Vector3f::CalculateAngleBetween(vec3fa,vec3fb);
-    BOOST_CHECK_EQUAL(angle,56.407);
-
+    Vector3f vec3fa(1.0f,0.0f,0.0f);
+    Vector3f vec3fb(0.0f,1.0f,0.0f);
+    float expected = 90;
+    float epsilon = 0.0001;
+    float angle = Vector3f::CalculateAngleBetween(vec3fa,vec3fb) * (180 / 3.1415926f);
+    BOOST_CHECK(std::fabs(expected-angle) <= epsilon);
 }
+
+BOOST_AUTO_TEST_CASE(Vector3Angle2) {
+    Vector3f vec3fa(0.6,0.8,0);
+    Vector3f vec3fb(0,0,1);
+    float expected = 90;
+    float epsilon = 0.0001;
+    float angle = Vector3f::CalculateAngleBetween(vec3fa,vec3fb) * (180 / 3.1415926f);
+    // std::cout << angle << std::endl;
+    BOOST_CHECK(std::fabs(angle-expected) <= epsilon);
+}
+
+BOOST_AUTO_TEST_CASE(Vector3AbsDot) {
+    Vector3f vec3fa(3,-4,0);
+    Vector3f vec3fb(-1,2,0);
+    float absDot = Vector3f::AbsDot(vec3fa,vec3fb);
+    BOOST_CHECK_EQUAL(absDot,11);
+}
+
+// BOOST_AUTO_TEST_CASE(Vector3GramSchmidt) {
+//     Vector3f vec3fa(3,4,0);
+//     Vector3f vec3fb(0,0,1);
+//     Vector3f vec3fc = Vector3f::GramSchmidt(vec3fa,vec3fb);
+//     Vector3f vec3fexpected = Vector3f(0,0,1);
+//     BOOST_CHECK(vec3fc == vec3fexpected);
+//     std::cout << vec3fc << std::endl;
+// }
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
