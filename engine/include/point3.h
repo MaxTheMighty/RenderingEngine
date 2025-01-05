06c6b52417acc6ed2a880acd5afc2d9d345e98f2
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-04 17:14:06 -0800

    implemented point3 functions and tests

--- engine/include/point3.h
+++ engine/include/point3.h
@@ -5,13 +5,54 @@
 #ifndef POINT3_H
 #define POINT3_H
 
+
 #include "tuple3.h"
+template <typename T>
+class Vector3;
 
 template <typename T>
 class Point3: public Tuple3<Point3, T> {
-    public:
-      Point3(): Tuple3<Point3,T>(){};
-      Point3(T x, T y, T z): Tuple3<Point3,T>(x,y,z){};
+public:
+    using Tuple3<Point3, T>::x;
+    using Tuple3<Point3, T>::y;
+    using Tuple3<Point3, T>::z;
+    using Tuple3<Point3, T>::operator+;
+
+
+    Point3(): Tuple3<Point3,T>(){};
+    Point3(T x, T y, T z): Tuple3<Point3,T>(x,y,z){};
+
+    template<typename U>
+    explicit Point3(Point3<U> p) : Tuple3<Point3,T>(T(p.x),T(p.y),T(p.z)){};
+
+    template<typename U>
+    explicit Point3(Vector3<U> v) : Tuple3<Point3,T>(T(v.x),T(v.y),T(v.z)){};
+
+    template<typename U>
+    auto operator+(Vector3<U> v) const -> Point3<decltype(T{} + U{})> {
+        return {x + v.x,y + v.y, z + v.z};
+    }
+
+    template<typename U>
+    Point3 &operator+=(Vector3<U> v) {
+        x+=v.x;
+        y+=v.y;
+        z+=v.z;
+        return *this;
+    }
+
+    template<typename U>
+    auto operator-(Point3<U> v) const -> Vector3<decltype(T{} + U{})> {
+        return {x - v.x, y - v.y, z - v.z};
+    }
+
+    static auto Distance(Point3<T> p1, Point3<T> p2) {
+        return Vector3<T>::Length(p1-p2);
+    }
+
+    static auto DistanceSquared(Point3<T> p1, Point3<T> p2) {
+        return Vector3<T>::LengthSquared(p1-p2);
+    }
 
 };
 
--- tests/src/point_tests.cpp
+++ tests/src/point_tests.cpp
@@ -24,6 +24,57 @@ BOOST_AUTO_TEST_CASE(Point3Constructor) {
     BOOST_CHECK_EQUAL(p3f.z,303.3f);
 };
 
+BOOST_AUTO_TEST_CASE(Point3FromPoint3) {
+    Point3i p3i = Point3i(Point3f(1.2f,2.6f,5.0f));
+    BOOST_CHECK_EQUAL(p3i.x,1);
+    BOOST_CHECK_EQUAL(p3i.y,2);
+    BOOST_CHECK_EQUAL(p3i.z,5);
+}
+
+BOOST_AUTO_TEST_CASE(Vector3ToPoint) {
+    Vector3f v3f(1.1,2.2,3.3);
+    Point3f p3f(v3f);
+    BOOST_CHECK(p3f == Point3f(1.1,2.2,3.3));
+}
+
+BOOST_AUTO_TEST_CASE(Point3Addition) {
+    Point3f p3f(1.1,2.2,3.3);
+    Point3f p3f2(500.0,0.5,100.0);
+    Point3f p3f3 = p3f + p3f2;
+    BOOST_CHECK(p3f3 == Point3f(501.1,2.7,103.3));
+}
+
+BOOST_AUTO_TEST_CASE(Point3InplaceAddition) {
+    Point3f p3f(1.0,2.5,3.9);
+    Vector3i v3i(1,2,3);
+    p3f+=v3i;
+    BOOST_CHECK(p3f==Point3f(2.0,4.5,6.9));
+}
+
+BOOST_AUTO_TEST_CASE(Point3Subtraction) {
+    Vector3f v3f = Point3f(5.0,4.0,3.0) - Point3f(1.0,2.0,3.0);
+    BOOST_CHECK(v3f == Vector3f(4.0,2.0,0.0));
+}
+
+BOOST_AUTO_TEST_CASE(Point3Distance) {
+    Point3f p3fa(4.0,3.0,7.0);
+    Point3f p3fb(9.0,100.0,200.0);
+    float distance = Point3f::Distance(p3fa,p3fb);
+    float expected = 216.06249096037;
+    float epsilon = 0.0001;
+    BOOST_CHECK(fabs(distance-expected) <= epsilon);
+
+}
+
+BOOST_AUTO_TEST_CASE(Point3DistanceSquared) {
+    Point3f p3fa(4.0,3.0,7.0);
+    Point3f p3fb(9.0,100.0,200.0);
+    float distanceSquared = Point3f::DistanceSquared(p3fa,p3fb);
+    float expected = 46682.9995850001;
+    float epsilon = 0.00001;
+    BOOST_CHECK(fabs(distanceSquared-expected) <= epsilon);
+}
+
 
 
 
