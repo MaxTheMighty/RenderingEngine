c08c8e2c6401e8d3e73fcf0ad3f70418b9d96cff
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-04 17:14:20 -0800

    cleaned up tuple3

--- engine/include/tuple3.h
+++ engine/include/tuple3.h
@@ -10,95 +10,95 @@
 
 template <template <typename> class Child, typename T>
 class Tuple3 {
-public:
-    T x{}, y{}, z{};
-    Tuple3(T x, T y, T z) : x{x}, y{y}, z{z} {
-        assert(x != NAN);
-        assert(y != NAN);
-        assert(z != NAN);
-    };
+    public:
 
-    Tuple3() : x{}, y{}, z{} {};
-    bool HasNaN() const { return IsNaN(x) || IsNaN(y) || IsNaN(z);}
+        Tuple3() = default;
+        Tuple3(T x, T y, T z) : x(x), y(y), z(z) {
+            assert(x != NAN);
+            assert(y != NAN);
+            assert(z != NAN);
+        };
 
-    T operator[](int i) const {
-        if (i == 0) return x;
-        if (i == 1) return y;
-        return z;
-    }
+        bool HasNaN() const { return IsNaN(x) || IsNaN(y) || IsNaN(z);}
 
-    T &operator[](int i) {
-        if (i == 0) return x;
-        if (i == 1) return y;
-        return z;
-    }
+        T operator[](int i) const {
+            if (i == 0) return x;
+            if (i == 1) return y;
+            return z;
+        }
 
-    bool operator==(Child<T> c) {
-        return x == c.x && y == c.y && z == c.z;
-    }
+        T &operator[](int i) {
+            if (i == 0) return x;
+            if (i == 1) return y;
+            return z;
+        }
 
-    bool operator!=(Child<T> c) {
-        return x != c.x || y != c.y || z != c.z;
-    }
+        bool operator==(Child<T> c) {
+            return x == c.x && y == c.y && z == c.z;
+        }
 
-    // This function returns a new Tuple of type Child with an inner type
-    // That is decided by C++'s type promotion rules (purpose for decltype)
-    template <typename U>
-    auto operator+(Child<U> c) const -> Child<decltype(T{} + U{})> {
-        return {x+c.x, y+c.y, z+c.z};
-    }
+        bool operator!=(Child<T> c) {
+            return x != c.x || y != c.y || z != c.z;
+        }
 
-    template <typename U>
-    auto operator-(Child<U> c) const -> Child<decltype(T{} + U{})> {
-        return {x-c.x, y-c.y, z-c.z};
-    }
+        // This function returns a new Tuple of type Child with an inner type
+        // That is decided by C++'s type promotion rules (purpose for decltype)
+        template <typename U>
+        auto operator+(Child<U> c) const -> Child<decltype(T{} + U{})> {
+            return {x+c.x, y+c.y, z+c.z};
+        }
 
-    // Do we need to do type matching here?
-    template <typename U>
-    Child<T> &operator+=(Child <U> c){
-        x+=c.x;
-        y+=c.y;
-        z+=c.z;
-        return static_cast<Child<T> &>(*this);
-    }
+        template <typename U>
+        auto operator-(Child<U> c) const -> Child<decltype(T{} + U{})> {
+            return {x-c.x, y-c.y, z-c.z};
+        }
 
-
-    template <typename U>
-    auto operator*(U c) const -> Child<decltype(T{} * U{})> {
-        return {x*c, y*c, z*c};
-    }
+        // Do we need to do type matching here?
+        template <typename U>
+        Child<T> &operator+=(Child <U> c){
+            x+=c.x;
+            y+=c.y;
+            z+=c.z;
+            return static_cast<Child<T> &>(*this);
+        }
 
 
+        template <typename U>
+        auto operator*(U c) const -> Child<decltype(T{} * U{})> {
+            return {x*c, y*c, z*c};
+        }
+
+        template <typename U>
+        Child<T> &operator*=(U c){
+            x*=c;
+            y*=c;
+            z*=c;
+            return static_cast<Child<T> &>(*this);
+        }
 
 
-    template <typename U>
-    Child<T> &operator*=(U c){
-        x*=c;
-        y*=c;
-        z*=c;
-        return static_cast<Child<T> &>(*this);
-    }
+        template <typename U>
+        auto operator/(U c) const -> Child<decltype(T{} + U{})>{
+            return {x/c, y/c, z/c};
+        }
 
+        template <typename U>
+        Child<T> &operator/=(U c){
+            x/=c;
+            y/=c;
+            z/=c;
+            return static_cast<Child<T> &>(*this);
+        }
 
-    template <typename U>
-    auto operator/(U c) const -> Child<decltype(T{} + U{})>{
-        return {x/c, y/c, z/c};
-    }
+        friend std::ostream & operator<<(std::ostream &os, const Tuple3 &obj) {
+            return os
+                   << "x: " << obj.x
+                   << " y: " << obj.y
+                   << " z: " << obj.z;
+        }
 
-    template <typename U>
-    Child<T> &operator/=(U c){
-        x/=c;
-        y/=c;
-        z/=c;
-        return static_cast<Child<T> &>(*this);
-    }
+        T x{}, y{}, z{};
 
-    friend std::ostream & operator<<(std::ostream &os, const Tuple3 &obj) {
-        return os
-               << "x: " << obj.x
-               << " y: " << obj.y
-               << " z: " << obj.z;
-    }
 };
 
 // Tuple3 Inline Functions
@@ -106,5 +106,9 @@ template <template <class> class C, typename T, typename U>
 inline auto operator*(U s, Tuple3<C, T> t) -> C<decltype(T{} * U{})> {
     return t * s;
 }
+
+
+
+
 
 #endif //TUPLE3_H
