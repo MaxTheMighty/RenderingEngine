c7fae3f0bf63827588d281a713adc1d753cc4b7e
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-06 16:32:17 -0800

    implemented a ray differential class

--- engine/include/ray.h
+++ engine/include/ray.h
@@ -16,8 +16,7 @@ class Ray {
         Vector3f d;
         float time = 0;
         Medium medium; // implemented later
-        Ray(const Point3f o, const Vector3f d) : o(o), d(d) {};
-        Ray(): o(), d(), time(){};
+        Ray(const Point3f o, const Vector3f d, float time = 0.f) : o(o), d(d), time(time) {};
 
         Point3f operator()(const float t) const {
             return o + (t * d);
@@ -25,6 +24,22 @@ class Ray {
 
 
 
+};
+
+class RayDifferential : public Ray {
+    public:
+        bool hasDifferentials = false;
+        Point3f rxOrigin, ryOrigin;
+        Vector3f rxDirection, ryDirection;
+        RayDifferential(const Point3f o, const Vector3f d, float time = 0.f): Ray(o,d,time){};
+        explicit RayDifferential(const Ray &ray) : Ray(ray) {};
+
+        void ScaleDifferentials(float s) {
+            rxOrigin = o + (rxOrigin - o) * s;
+            ryOrigin = o + (ryOrigin - o) * s;
+            rxDirection = d + (rxDirection - d) * s;
+            ryDirection = d + (ryDirection - d) * s;
+        }
 };
 
 
