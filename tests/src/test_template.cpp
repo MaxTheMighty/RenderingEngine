d9f65a745b5ad664109115dc8f82c2e683f44f51
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-11 10:00:02 -0800

    CMakeLists updates for new files and test template created

---
+++ tests/src/test_template.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Maxwell Kozlov on 1/11/25.
+//
+
+#include <boost/test/unit_test.hpp>
+#include "../../engine/include/vector3.h"
+#define BOOST_TEST_DYN_LINK
+
+
+BOOST_AUTO_TEST_SUITE(_);
+
+BOOST_AUTO_TEST_CASE(_) {
+
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
