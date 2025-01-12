e1752e80069013bc1b11a6bd2e2a5bb145fbb24b
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-12 14:29:32 -0800

    wrote Bounds3.h interaction functions and tests

--- engine/include/bounds3.h
+++ engine/include/bounds3.h
@@ -18,6 +18,46 @@ class Bounds3{
     }
     explicit Bounds3(Point3<T> p1, Point3<T> p2) : pMin(Point3<T>::Min(p1,p2)), pMax(Point3<T>::Max(p1,p2)) {};
     explicit Bounds3(Point3<T> p) : pMin(p), pMax(p) {};
+    Point3<T> operator[](const int i) const {return (i == 0 ? pMin : pMax);}
+    Point3<T> &operator[](const int i) { return (i == 0 ? pMin : pMax);}
+    Point3<T> Corner(const int corner) const {
+      return Point3<T>(
+        (*this)[corner & 1].x,
+        (*this)[corner & 2 ? 1 : 0].y,
+        (*this)[corner & 4 ? 1 : 0].z
+      );
+    }
+
+    static Bounds3<T> Union(const Bounds3<T> &b, const Point3<T> p) {
+      Bounds3<T> boundsOut;
+      boundsOut.pMin = Point3<T>::Min(p,b.pMin);
+      boundsOut.pMax = Point3<T>::Max(p,b.pMax);
+      return boundsOut;
+    }
+
+    static Bounds3<T> Union(const Bounds3<T> b1, const Bounds3<T> &b2) {
+      Bounds3<T> boundsOut;
+      boundsOut.pMin = Point3<T>::Min(b1.pMin,b2.pMin);
+      boundsOut.pMax = Point3<T>::Max(b1.pMax,b2.pMax);
+      return boundsOut;
+    }
+
+
+    static Bounds3<T> Intersection(const Bounds3<T> &b1, const Bounds3<T> &b2) {
+      Bounds3<T> boundsOut;
+      boundsOut.pMin = Point3<T>::Max(b1.pMin,b2.pMin);
+      boundsOut.pMax = Point3<T>::Min(b1.pMax,b2.pMax);
+      return boundsOut;
+    }
+
+    static bool Overlap(const Bounds3<T> &b1, const Bounds3<T> &b2) {
+        bool xOverlap = (b2.pMin.x <= b1.pMax.x) && (b1.pMin.x <= b2.pMax.x);
+        bool yOverlap = (b2.pMin.y <= b1.pMax.y) && (b1.pMin.y <= b2.pMax.y);
+        bool zOverlap = (b2.pMin.z <= b1.pMax.z) && (b1.pMin.z <= b2.pMax.z);
+        return xOverlap && yOverlap && zOverlap;
+    }
+
+    //
 
 
 
--- tests/src/bounds_tests.cpp
+++ tests/src/bounds_tests.cpp
@@ -30,7 +30,72 @@ BOOST_AUTO_TEST_CASE(BoundsOnePointConstructor) {
 
 }
 
+BOOST_AUTO_TEST_CASE(BoundsIndexOperator) {
+    Point3f pMin = Point3f(0,0,0);
+    Point3f pMax = Point3f(100,100,100);
+    Bounds3f bounds  = Bounds3f(pMin,pMax);
+    BOOST_CHECK(bounds[0] == pMin);
+    BOOST_CHECK(bounds[1] == pMax);
+}
 
+BOOST_AUTO_TEST_CASE(BoundsCorner) {
+    Point3f pMin = Point3f(0,0,0);
+    Point3f pMax = Point3f(100,100,100);
+    Bounds3f bounds  = Bounds3f(pMin,pMax);
+    BOOST_CHECK(bounds.Corner(0) == pMin);
+    BOOST_CHECK(bounds.Corner(1) == Point3f(100,0,0));
+    BOOST_CHECK(bounds.Corner(6) == Point3f(0,100,100));
+    BOOST_CHECK(bounds.Corner(7) == pMax);
+}
 
+BOOST_AUTO_TEST_CASE(BoundsUnion) {
+    Bounds3f bounds = Bounds3f(Point3f(0,0,0),Point3f(100,100,100));
+    Point3f p = Point3f(-100.0,250.0,500.0);
+    bounds = Bounds3f::Union(bounds,p);
+    BOOST_CHECK(bounds[0] == Point3f(-100,0.0,0.0));
+    BOOST_CHECK(bounds[1] == Point3f(100.0,250.0,500.0));
+}
+
+BOOST_AUTO_TEST_CASE(BoundsUnionNoChange) {
+    Bounds3f bounds = Bounds3f(Point3f(0,0,0),Point3f(100,100,100));
+    Point3f p = Point3f(50.0,50.0,50.0);
+    bounds = Bounds3f::Union(bounds,p);
+    BOOST_CHECK(bounds[0] == Point3f(0.0,0.0,0.0));
+    BOOST_CHECK(bounds[1] == Point3f(100.0,100.0,100.0));
+}
+
+BOOST_AUTO_TEST_CASE(BoundsUnionBounds) {
+    Bounds3f bounds = Bounds3f(Point3f(0,0,0),Point3f(100,100,100));
+    Bounds3f bounds2 = Bounds3f(Point3f(20,20,20),Point3f(150,150,150));
+    Bounds3f boundsUnion = Bounds3f::Union(bounds,bounds2);
+    BOOST_CHECK(boundsUnion[0] == Point3f(0,0,0));
+    BOOST_CHECK(boundsUnion[1] == Point3f(150,150,150));
+}
+
+BOOST_AUTO_TEST_CASE(BoundsIntersection) {
+    Bounds3f b1(Point3f(0,0,0),Point3f(4,4,0));
+    Bounds3f b2(Point3f(1,-1,0),Point3f(5,3,0));
+    Bounds3f intersection = Bounds3f::Intersection(b1,b2);
+    BOOST_CHECK(intersection[0] == Point3f(1,0,0));
+    BOOST_CHECK(intersection[1] == Point3f(4,3,0));
+}
+
+BOOST_AUTO_TEST_CASE(BoundsOverlap) {
+    Bounds3f b1(Point3f(0,0,0),Point3f(4,4,0));
+    Bounds3f b2(Point3f(1,-1,0),Point3f(5,3,0));
+    BOOST_CHECK(Bounds3f::Overlap(b1,b2));
+}
+
+BOOST_AUTO_TEST_CASE(BoundsDontOverlap) {
+    Bounds3f b1(Point3f(0,0,2),Point3f(4,4,4));
+    Bounds3f b2(Point3f(1,-1,0),Point3f(5,3,0));
+    BOOST_CHECK(!Bounds3f::Overlap(b1,b2));
+}
+
+BOOST_AUTO_TEST_CASE(BoundsDontOverlap2) {
+    Bounds3f b1(Point3f(0,0,0),Point3f(4,4,0));
+    Bounds3f b2(Point3f(1,6,0),Point3f(5,8,0));
+    BOOST_CHECK(!Bounds3f::Overlap(b1,b2));
+}
 BOOST_AUTO_TEST_SUITE_END()
 
