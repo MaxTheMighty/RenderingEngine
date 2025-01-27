668595cc5798b38138442aa8230f730d3adef00e
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2025-01-26 16:57:39 -0800

    main created to test out canvas

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,25 +1,68 @@
+//
+// Created by Maxwell Kozlov on 1/25/25.
+//
+
 #include <iostream>
+#include <SDL3/sdl.h>
+#include "SDL3/SDL_events.h"
+#include "include/canvas.h"
+#include "include/parser.h"
+//Screen dimension constants
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 1000;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+  
+
+    Model model; 
+    model = Parser::ParseWavefront("obj/face.obj");
+
+
+    Canvas canvas("Rendering engine",SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_RESIZABLE);
+    SDL_Event event;
+    while (true){
+        while (SDL_PollEvent(&event) != false) {
+            if (event.type == SDL_EVENT_QUIT) {
+                return -1;
+            }
+        }
+    canvas.RenderClearColor(0,0,0,255);
+    canvas.SetColor(255,255,255,255);
+    for(int i = 0; i < model.edges.size(); i++){
+        Vector3i vertex_index = model.edges[i];
+        Point3f edge_a = model.vertices[vertex_index.x-1];
+        Point3f edge_b = model.vertices[vertex_index.y-1];
+        Point3f edge_c = model.vertices[vertex_index.z-1];
+
+      // normalize
+        edge_a = edge_a + 1;
+        edge_a /= 2;
+        edge_a.x *= SCREEN_WIDTH;
+        edge_a.y *= SCREEN_HEIGHT;
+        edge_a.y = SCREEN_HEIGHT - edge_a.y;
+
+        edge_b = edge_b + 1;
+        edge_b /= 2;
+        edge_b.x *= SCREEN_WIDTH;
+        edge_b.y *= SCREEN_HEIGHT;
+        edge_b.y = SCREEN_HEIGHT - edge_b.y;
+
+        edge_c = edge_c + 1;
+        edge_c /= 2;
+        edge_c.x *= SCREEN_WIDTH;
+        edge_c.y *= SCREEN_HEIGHT;
+        edge_c.y = SCREEN_HEIGHT - edge_c.y;
+
+
+        canvas.DrawLine(edge_a.x,edge_a.y,edge_b.x,edge_b.y);
+        canvas.DrawLine(edge_b.x,edge_b.y,edge_c.x,edge_c.y);
+        canvas.DrawLine(edge_a.x,edge_a.y,edge_c.x,edge_c.y);
     }
 
-    return 0;
+    canvas.RenderPresent();
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+
+}
